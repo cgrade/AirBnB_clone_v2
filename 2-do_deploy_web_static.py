437f302fb76af3ea679f5cb01eab3018dbc729ef
Harley <111179132+HarleyOtos@users.noreply.github.com>
2023-04-05 23:12:36 +0100

    deploy archive

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,34 +1,65 @@
 #!/usr/bin/python3
-from fabric.api import *
-import os
+
+from datetime import datetime
+from fabric.api import env, local, put, run
+from os import path
 
 
 env.hosts = ['54.224.21.248', '100.25.28.51']
 env.user = 'ubuntu'
 
 
+def do_pack():
+    """
+    Compress web_static folder contents into a tgz archive
+    """
+
+    # create the directory if it doesn't exist
+    local("mkdir -p versions")
+
+    # create the file name using the current date and time
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    archive_name = "web_static_{}.tgz".format(timestamp)
+
+    # compress the contents of web_static and save in versions directory
+    local("tar -czvf versions/{} web_static".format(archive_name))
+
+    # return the path of the archive
+    return path.join("versions", archive_name)
+
+
 def do_deploy(archive_path):
-    if not os.path.exists(archive_path):
+    """
+    Distributes an archive to the web servers
+    """
+
+    if not path.exists(archive_path):
         return False
 
-    # Upload the archive to the /tmp/ directory of the web server
-    put(archive_path, '/tmp/')
+    # get the file name and directory name from the archive path
+    file_name = path.basename(archive_path)
+    dir_name = path.splitext(file_name)[0]
 
-    # Get the archive filename without extension
-    archive_filename = os.path.basename(archive_path)
-    archive_filename_noext = os.path.splitext(archive_filename)[0]
+    # upload the archive to the /tmp/ directory of the web server
+    put(archive_path, "/tmp/{}".format(file_name))
 
-    # Uncompress the archive to the folder /data/web_static/releases/<archive filename without extension> on the web server
-    run('mkdir -p /data/web_static/releases/{}'.format(archive_filename_noext))
-    run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(archive_filename, archive_filename_noext))
+    # create the directory for the new version
+    run("mkdir -p /data/web_static/releases/{}/".format(dir_name))
 
-    # Delete the archive from the web server
-    run('rm /tmp/{}'.format(archive_filename))
+    # extract the contents of the archive into the new directory
+    run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(file_name, dir_name))
 
-    # Delete the symbolic link /data/web_static/current from the web server
-    run('rm -f /data/web_static/current')
+    # delete the archive from the server
+    run("rm /tmp/{}".format(file_name))
 
-    # Create a new the symbolic link /data/web_static/current on the web server, linked to the new version of your code (/data/web_static/releases/<archive filename without extension>)
-    run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(archive_filename_noext))
+    # move the contents of web_static into the new directory
+    run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(dir_name, dir_name))
+
+    # remove the old web_static directory
+    run("rm -rf /data/web_static/releases/{}/web_static".format(dir_name))
+
+    # delete and recreate the symbolic link to the new version
+    run("rm -rf /data/web_static/current")
+    run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(dir_name))
 
     return True
