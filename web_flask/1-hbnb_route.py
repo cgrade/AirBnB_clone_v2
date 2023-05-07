8f83bb9546977e3a405e5a8cbc57fe82145ec08b
cgrade <gradeisageek@gmail.com>
2023-05-07 23:58:38 +0100

    task 1 added

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" A python Script that starts a Flask web app
+    and listens on 0.0.0.0, port 5000
+"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False):
+    """ A route that displays "Hello HBNB" """
+    return ("Hello HBNB!")
+
+
+@app.route('/hbnb', strict_slashes=False):
+    """ A route that displays 'HBNB' """
+    return ("HBNB")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000. debug=None)
