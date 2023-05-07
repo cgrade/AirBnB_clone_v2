3a7a6eb7183e9591f3b324e3fd84de00042ad0a6
cgrade <gradeisageek@gmail.com>
2023-05-08 00:33:49 +0100

    task 2 added

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""A Flask web app that uses dynamic variable"""
+
+from flask import Flask
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """return a strings"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Return a string"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def dynamic_text(text):
+    """ Return the given variable"""
+    return (i'C is %s' % escape(text))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=None)
