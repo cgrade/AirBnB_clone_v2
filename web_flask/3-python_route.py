279504c03c050e9989941814033c28f168dbb5a0
cgrade <gradeisageek@gmail.com>
2023-05-08 14:35:11 +0100

    task 3 solution updated

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -5,7 +5,7 @@ and displays a dynamic content based on the variable in the url routing
 
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask("__name__")
 
 
 @app.route('/', strict_slashes=False)
